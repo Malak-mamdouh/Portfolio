b1ccd9c5bf669f665951e2cc84df7fca6ff11068
Malak Mamdouh <malakmamdouh80@gmail.com>
2022-05-24 21:13:46 +0200

    add-links-to-icons

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 /* Initialize variables */
 const listIcon = document.querySelector(".list-icon");
 const menuPhone = document.querySelector(".mobile-menu");
-var typed = new Typed("#name" ,  { 
-      strings: ["Malak Mamdouh"] , 
-      typeSpeed: 150 , 
-      backSpeed: 150 , 
-      loop: true 
-});
-
+const allIconBox = document.querySelectorAll(".icon-box");
+const section = document.querySelector("section");
 
 /****  Functions  ***/
 listIcon.addEventListener("click" , function() {
       listIcon.classList.toggle("active");
       menuPhone.classList.toggle("open");
 });
+
+
